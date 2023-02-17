96b49f7ab5e3f688d4502826e8b871194dce7063
Manuel Cabeza Gallucci <42854238+manuelcgallucci@users.noreply.github.com>
2023-02-17 22:59:33 +0100

    corrected train

--- train_model.py
+++ train_model.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Fri Oct 28 21:12:45 2022
 
@@ -9,16 +8,17 @@ Added:
    - Calculate and use s2 in loss_reg
    - Normalized both losses
 """
-
 import torch 
 from torch.utils.data import DataLoader
 import numpy as np
-import matplotlib.pyplot as plt
+from time import time
+import os 
 
 import dataloader as dl
 import nn_definitions as nn_d
 import utility as ut
 from model_3 import CNNGenerator
+
 
 def train_model( lr, epochs, batch_size, k_epochs_d, alpha, beta, gamma, out_dir, noise_size=(1,2**15)):
     
@@ -48,7 +48,8 @@ def train_model( lr, epochs, batch_size, k_epochs_d, alpha, beta, gamma, out_dir
     optim_g = torch.optim.Adam(generator.parameters(),lr= lr, betas=(0.5, 0.999))
 
     # Train dataset 
-    data_train = torch.Tensor(np.genfromtxt('data_train.csv', delimiter=',')) # Nsamples x L (L: length)
+    data_train = torch.Tensor(np.load('./data/data.npy')) # Nsamples x L (L: length)
+    data_train = data_train[:,None,:]
     data_samples = data_train.size()[0]
     data_len = data_train.size()[1]
 
@@ -76,30 +77,31 @@ def train_model( lr, epochs, batch_size, k_epochs_d, alpha, beta, gamma, out_dir
     scales=np.unique(uu.astype(int))
     scales=scales[0:100]
     
-    s2 = ut.calculate_s2(torch.cumsum(data_train[:,None,:], dim=2), scales, device=device)
+    # s2 = ut.calculate_s2(torch.cumsum(data_train[:,None,:], dim=2), scales, device=device)
+    s2 = ut.calculate_s2(data_train, scales, device=device)
     mean_s2 = torch.mean(s2, dim=[0,1]) # This gives the s2 tensor of size (len(scales))
     mean_s2 = mean_s2[None, None, :] 
+
+    # Take the target ones and zeros for the batch size and for the last (not complete batch)
+
+    target_ones_full = torch.ones((batch_size, 1), device=device)
+    target_ones = [target_ones_full, target_ones_partial]
+    target_ones_partial = torch.ones((data_samples - batch_size * np.floor(data_samples / batch_size), 1), device=device)
+
+    target_zeros_full = torch.zeros((batch_size, 1), device=device)
+    target_zeros_partial = torch.ones((data_samples - batch_size * np.floor(data_samples / batch_size), 1), device=device)
+    target_zeros = [target_zeros_full, target_zeros_partial]
+    
+    last_batch_idx = np.ceil(data_samples / batch_size) - 1
+    
+    start_time = time()
     for epoch in range(epochs):
-        
-        running_loss_gen = 0 
-        running_loss_g = 0
-        running_loss_reg = 0
-
-        running_loss_disc = 0
-        running_loss_fake = 0
-        running_loss_real = 0
-
-        running_acc_fake_disc = 0
-        running_acc_real_disc = 0
 
         for batch_idx, data_ in enumerate(train_loader):
 
             data_ = data_.to(device).float()
             # data_ = torch.unsqueeze(data_, dim=1)
             batch_size_ = data_.shape[0]
-                
-            target_ones = torch.ones((batch_size_, 1), device=device)
-            target_zeros = torch.zeros((batch_size_, 1), device=device)
 
             ## TRAIN DISCRIMINATOR
             for k in range(k_epochs_d):
@@ -108,7 +110,7 @@ def train_model( lr, epochs, batch_size, k_epochs_d, alpha, beta, gamma, out_dir
                 # optim_d.zero_grad()
                 ## True samples
                 pred_real = discriminator(data_)
-                loss_real = criterion_BCE(pred_real, target_ones)
+                loss_real = criterion_BCE(pred_real, target_ones[batch_idx == last_batch_idx])
 
                 ## False samples (Create random noise and run the generator on them)
                 noise = torch.randn((batch_size_, noise_size[0], noise_size[1]), device=device)
@@ -116,7 +118,7 @@ def train_model( lr, epochs, batch_size, k_epochs_d, alpha, beta, gamma, out_dir
                     fake_samples = generator(noise)
                                     
                 pred_fake = discriminator(fake_samples)
-                loss_fake = criterion_BCE(pred_fake, target_zeros)
+                loss_fake = criterion_BCE(pred_fake, target_zeros[batch_idx == last_batch_idx])
                 
                 # Combine the losses 
                 loss_d = gamma * (loss_real + loss_fake) / 2
@@ -128,14 +130,14 @@ def train_model( lr, epochs, batch_size, k_epochs_d, alpha, beta, gamma, out_dir
                 # Discriminator optimizer step
                 optim_d.step()
                 
-                running_loss_real += loss_real / k_epochs_d
-                running_loss_fake += loss_fake / k_epochs_d
-                running_loss_disc += loss_d / k_epochs_d
+                loss_d_real_array[epoch] += loss_real / k_epochs_d
+                loss_d_fake_array[epoch] += loss_fake / k_epochs_d
+                loss_d_array[epoch] += loss_d / k_epochs_d
 
             # If pred_fake is all zeros then acc should be 1.0
             # We want this to be around 0.5. 1.0 means perfect accuracy (the generated samples are not similar to the samples)
-            running_acc_fake_disc += torch.sum(pred_fake < 0.5)
-            running_acc_real_disc += torch.sum(pred_real >= 0.5)
+            acc_d_fake_array[epoch] += torch.sum(pred_fake < 0.5)
+            acc_d_real_array[epoch] += torch.sum(pred_real >= 0.5)
 
             #optim_g.zero_grad()
             ## TRAIN GENERATOR
@@ -146,7 +148,6 @@ def train_model( lr, epochs, batch_size, k_epochs_d, alpha, beta, gamma, out_dir
             # Cut samples (no)
             
             classifications = discriminator(generated_signal)
-            
             loss_g = criterion_BCE(classifications, target_ones)
 
             # E [( X * cumsum(Z) ) ^2]
@@ -158,33 +159,30 @@ def train_model( lr, epochs, batch_size, k_epochs_d, alpha, beta, gamma, out_dir
             
             # Structure function loss
             # Broadcast s2 into all structure function estimations
-            generated_s2 = ut.calculate_s2_v2(torch.cumsum(generated_signal,dim=2), scales, device=device)
-            loss_reg = criterion_MSE(generated_s2, mean_s2)
-
-            loss_gen = beta * ( alpha_comp * loss_g + alpha *  loss_reg )
+            # generated_s2 = ut.calculate_s2(torch.cumsum(generated_signal,dim=2), scales, device=device)
+            # loss_reg = criterion_MSE(generated_s2, mean_s2)
             
-            running_loss_gen += loss_gen.item()
-            running_loss_reg += loss_reg.item()
-            running_loss_g += loss_g.item()
+            # loss_gen = beta * ( alpha_comp * loss_g + alpha *  loss_reg )
+            
+            loss_gen = beta * loss_g
+
+            loss_gen_array[epoch] += loss_gen.item()
+            # loss_reg_array[epoch] += loss_reg.item()
+            loss_g_array[epoch] += loss_g.item()
 
             loss_gen.backward()
             optim_g.step()
-            
+
         # THESE HAVE TO BE DEVIDED BY THE NUMBER OF BATCHES
-        loss_g_array[epoch] = running_loss_g
-        loss_reg_array[epoch] = running_loss_reg
-        loss_gen_array[epoch] = running_loss_gen           
-
-        loss_d_fake_array[epoch] = running_loss_fake
-        loss_d_real_array[epoch] = running_loss_real
-        loss_d_array[epoch] = running_loss_disc
-
-        acc_d_fake_array[epoch] = running_acc_fake_disc / data_samples
-        acc_d_real_array[epoch] = running_acc_real_disc / data_samples
+        acc_d_fake_array[epoch] = acc_d_fake_array[epoch] / data_samples
+        acc_d_real_array[epoch] = acc_d_real_array[epoch] / data_samples
 
         if epoch%1 == 0:
-            print('Epoch [{}/{}] -\t Generator Loss: {:7.4f} \t/\t\t Discriminator Loss: {:7.4f} || acc(fake): {:7.4f} , acc(real): {:7.4f} ||'.format(epoch+1, epochs, running_loss_gen, running_loss_disc, acc_d_fake_array[epoch], acc_d_real_array[epoch]))
-            print("\t\t\t G: {:7.4f}, reg: {:7.4f} \t\t Fake: {:7.4f}, Real: {:7.4f}".format(running_loss_g, running_loss_reg, running_loss_fake, running_loss_real))
+            print('Epoch [{}/{}] -\t Generator Loss: {:7.4f} \t/\t\t Discriminator Loss: {:7.4f} || acc(fake): {:7.4f} , acc(real): {:7.4f} ||'.format(epoch+1, epochs, loss_gen_array[epoch], loss_d_array[epoch], acc_d_fake_array[epoch], acc_d_real_array[epoch]))
+            print("\t\t\t G: {:7.4f}, reg: {:7.4f} \t\t Fake: {:7.4f}, Real: {:7.4f}".format(loss_g_array[epoch], loss_reg_array[epoch], loss_d_fake_array[epoch], loss_d_real_array[epoch]))
+
+    end_time = time()
+    print("Total time elapsed for training:", end_time - start_time)
 
     n_samples = 64 # Generate 64 samples
     noise = torch.randn((n_samples, noise_size[0], noise_size[1]), device=device)
@@ -212,12 +210,15 @@ if __name__ == '__main__':
     batch_size = 32
     k_epochs_d = 2
     
-    out_dir = './gen_data/'
+    out_dir = './generated'
     alpha = 0.5 # regularization parameter
     beta = 0.01 # generator loss multiplier
     gamma = 1.0 # discriminator loss multiplier
 
-    meta_dir = {
+    # out_dir = ut.get_dir(out_dir)
+    out_dir = os.path.join(out_dir, 'DwqjD0')
+
+    meta_dict = {
         "lr":lr,
         "epochs":epochs,
         "batch_size":batch_size,
@@ -228,6 +229,7 @@ if __name__ == '__main__':
         "gamma":gamma
     }
 
-    train_model(lr, epochs, batch_size, k_epochs_d, alpha, beta, gamma, out_dir)
+    ut.save_meta(meta_dict, out_dir)
+    # train_model(lr, epochs, batch_size, k_epochs_d, alpha, beta, gamma, out_dir)
 
 
