3dab9f047044e2f1401a1a52137e99f0e9a0470b
Manuel Cabeza Gallucci <42854238+manuelcgallucci@users.noreply.github.com>
2023-02-17 22:59:35 +0100

    Update utility.py

--- utility.py
+++ utility.py
@@ -1,6 +1,8 @@
 
 import numpy as np
 import torch
+import random, string
+import os 
 
 # Tested at 50 tests, 32 batch size, 2**15 length, 100 scales
 # 0.017 gpu
@@ -21,3 +23,21 @@ def calculate_s2(signal, scales, device="cpu"):
         s2[:,:,idx] = torch.log(torch.mean(torch.square(tmp[:,:,scale:]-tmp[:,:,:-scale]), dim=2))
         
     return s2
+
+
+
+def get_dir(dir, length=6):
+    name = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+    path_ = os.path.join(dir, name)
+    while os.path.isfile(path_):
+        name = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+        path_ = os.path.join(dir, name)
+    os.mkdir(path_)
+    return path_
+
+def save_meta(meta_dict, meta_dir, meta_name="meta.txt"):
+    with open( os.path.join(meta_dir, meta_name), "w") as f:
+        for k, v in meta_dict.items():
+            f.write("{:s}: ".format(k) + str(v) + '\n')
+
+        f.write("\nreg: \n")
