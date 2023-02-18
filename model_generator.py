e53b0d922920a7353062a72c0b6ba5bbaf7166f4
Manuel CABEZA GALLUCCI <m21cabez@sl-tp-br-021.imta.fr>
2023-02-18 18:23:18 +0100

    defined new bigger models

--- model_generator.py
+++ model_generator.py
@@ -1,18 +1,20 @@
 from torch import nn
 
+
+
 ### Model version 3
-class CN1NGenerator(nn.Module):
+class CNNGeneratorBig1(nn.Module):
 	def __init__(self):
 		super().__init__()
 		self.avgPool2 = nn.AvgPool1d(2, ceil_mode=True)
 		self.upsample2 = nn.Upsample(scale_factor=2, mode='linear')
 		self.cnn1 = nn.Sequential(
-                 	nn.Conv1d(1, 16, kernel_size=1, stride=1, padding="same", bias=False),
+                 	nn.Conv1d(1, 16, kernel_size=1, stride=1, padding=0, bias=False),
                 	nn.BatchNorm1d(16),
                 	nn.ReLU(True),
                 )
 		self.cnn2 = nn.Sequential(
-                 	nn.Conv1d(16, 32, kernel_size=2, stride=1, padding="same", bias=False),
+                 	nn.Conv1d(16, 32, kernel_size=2, stride=1, padding=0, bias=False),
                 	nn.BatchNorm1d(32),
                 	nn.ReLU(True),
                 )
@@ -22,7 +24,7 @@ class CN1NGenerator(nn.Module):
                 	nn.ReLU(True),
                 )
 		self.cnn4 = nn.Sequential(
-                 	nn.Conv1d(32, 64, kernel_size=4, stride=1, padding="same", bias=False),
+                 	nn.Conv1d(32, 64, kernel_size=4, stride=1, padding=0, bias=False),
                 	nn.BatchNorm1d(64),
                 	nn.ReLU(True),
                 )
@@ -32,7 +34,7 @@ class CN1NGenerator(nn.Module):
                 	nn.ReLU(True),
                 )
 		self.cnn8 = nn.Sequential(
-                 	nn.Conv1d(64, 128, kernel_size=8, stride=1, padding="same", bias=False),
+                 	nn.Conv1d(64, 128, kernel_size=8, stride=1, padding=0, bias=False),
                 	nn.BatchNorm1d(128),
                 	nn.ReLU(True),
                 )
@@ -42,7 +44,7 @@ class CN1NGenerator(nn.Module):
                 	nn.ReLU(True),
                 )
 		self.cnn16 = nn.Sequential(
-                 	nn.Conv1d(128, 256, kernel_size=16, stride=1, padding="same", bias=False),
+                 	nn.Conv1d(128, 256, kernel_size=16, stride=1, padding=0, bias=False),
                 	nn.BatchNorm1d(256),
                 	nn.ReLU(True),
                 )
@@ -52,7 +54,7 @@ class CN1NGenerator(nn.Module):
                 	nn.ReLU(True),
                 )
 		self.cnn32 = nn.Sequential(
-                 	nn.Conv1d(256, 256, kernel_size=32, stride=1, padding="same", bias=False),
+                 	nn.Conv1d(256, 256, kernel_size=32, stride=1, padding=0, bias=False),
                 	nn.BatchNorm1d(256),
                 	nn.ReLU(True),
                 )
@@ -62,7 +64,7 @@ class CN1NGenerator(nn.Module):
                 	nn.ReLU(True),
                 )
 		self.cnn64 = nn.Sequential(
-                 	nn.Conv1d(256, 256, kernel_size=64, stride=1, padding="same", bias=False),
+                 	nn.Conv1d(256, 256, kernel_size=64, stride=1, padding=0, bias=False),
                 	nn.BatchNorm1d(256),
                 	nn.ReLU(True),
                 )
@@ -182,7 +184,7 @@ class CN1NGenerator(nn.Module):
 		# print(24, out.shape)
 		out = self.upsample2(out)
 		# print(25, out.shape)
-		out = out + res1616
+		out = out[:,:,0:-1] + res1616
 
 		out = self.cnnTrans16(out)
 		# print(26, out.shape)
@@ -194,13 +196,13 @@ class CN1NGenerator(nn.Module):
 		# print(28, out.shape)
 		out = self.upsample2(out)
 		# print(29, out.shape)
-		out = out + res44
+		out = out[:,:,0:-1] + res44
 
 		out = self.cnnTrans4(out)
 		# print(30, out.shape)
 		out = self.upsample2(out)
 		# print(31, out.shape)
-		out = out + res22
+		out = out[:,:,0:-1] + res22
 
 		out = self.cnnTrans2(out)
 		# print(32, out.shape)
@@ -213,11 +215,8 @@ class CN1NGenerator(nn.Module):
 		return out
 
 
-# Original model
-from torch import nn
-
 ### Model version 3
-class CNNGenerator(nn.Module):
+class CNNGeneratorBig2(nn.Module):
 	def __init__(self):
 		super().__init__()
 		self.avgPool2 = nn.AvgPool1d(2, ceil_mode=True)
@@ -232,8 +231,18 @@ class CNNGenerator(nn.Module):
                 	nn.BatchNorm1d(32),
                 	nn.ReLU(True),
                 )
+		self.cnn22 = nn.Sequential(
+                 	nn.Conv1d(32, 32, kernel_size=2, stride=1, padding="same", bias=False),
+                	nn.BatchNorm1d(32),
+                	nn.ReLU(True),
+                )
 		self.cnn4 = nn.Sequential(
                  	nn.Conv1d(32, 64, kernel_size=4, stride=1, padding=0, bias=False),
+                	nn.BatchNorm1d(64),
+                	nn.ReLU(True),
+                )
+		self.cnn44 = nn.Sequential(
+                 	nn.Conv1d(64, 64, kernel_size=4, stride=1, padding="same", bias=False),
                 	nn.BatchNorm1d(64),
                 	nn.ReLU(True),
                 )
@@ -242,13 +251,28 @@ class CNNGenerator(nn.Module):
                 	nn.BatchNorm1d(128),
                 	nn.ReLU(True),
                 )
+		self.cnn88 = nn.Sequential(
+                 	nn.Conv1d(128, 128, kernel_size=8, stride=1, padding="same", bias=False),
+                	nn.BatchNorm1d(128),
+                	nn.ReLU(True),
+                )
 		self.cnn16 = nn.Sequential(
                  	nn.Conv1d(128, 256, kernel_size=16, stride=1, padding=0, bias=False),
                 	nn.BatchNorm1d(256),
                 	nn.ReLU(True),
                 )
+		self.cnn1616 = nn.Sequential(
+                 	nn.Conv1d(256, 256, kernel_size=16, stride=1, padding="same", bias=False),
+                	nn.BatchNorm1d(256),
+                	nn.ReLU(True),
+                )
 		self.cnn32 = nn.Sequential(
                  	nn.Conv1d(256, 256, kernel_size=32, stride=1, padding=0, bias=False),
+                	nn.BatchNorm1d(256),
+                	nn.ReLU(True),
+                )
+		self.cnn3232 = nn.Sequential(
+                 	nn.Conv1d(256, 256, kernel_size=32, stride=1, padding="same", bias=False),
                 	nn.BatchNorm1d(256),
                 	nn.ReLU(True),
                 )
@@ -273,35 +297,35 @@ class CNNGenerator(nn.Module):
                 	nn.ReLU(True),
                 )
 		self.cnnTrans64 = nn.Sequential(
-             	nn.ConvTranspose1d(256, 256, kernel_size=64, stride=1, padding=0, bias=False),
-            	nn.BatchNorm1d(256),
-            	nn.ReLU(True),
-            )
+                 	nn.ConvTranspose1d(256, 256, kernel_size=64, stride=1, padding=0, bias=False),
+                	nn.BatchNorm1d(256),
+                	nn.ReLU(True),
+                )
 		self.cnnTrans32 = nn.Sequential(
-             	nn.ConvTranspose1d(256, 256, kernel_size=32, stride=1, padding=0, bias=False),
-            	nn.BatchNorm1d(256),
-            	nn.ReLU(True),
-            )
+                 	nn.ConvTranspose1d(256, 256, kernel_size=32, stride=1, padding=0, bias=False),
+                	nn.BatchNorm1d(256),
+                	nn.ReLU(True),
+                )
 		self.cnnTrans16 = nn.Sequential(
-             	nn.ConvTranspose1d(256, 128, kernel_size=16, stride=1, padding=0, bias=False),
-            	nn.BatchNorm1d(128),
-            	nn.ReLU(True),
-            )
+                 	nn.ConvTranspose1d(256, 128, kernel_size=16, stride=1, padding=0, bias=False),
+                	nn.BatchNorm1d(128),
+                	nn.ReLU(True),
+                )
 		self.cnnTrans8 = nn.Sequential(
-             	nn.ConvTranspose1d(128, 64, kernel_size=8, stride=1, padding=0, bias=False),
-            	nn.BatchNorm1d(64),
-            	nn.ReLU(True),
-            )
+                 	nn.ConvTranspose1d(128, 64, kernel_size=8, stride=1, padding=0, bias=False),
+                	nn.BatchNorm1d(64),
+                	nn.ReLU(True),
+                )
 		self.cnnTrans4 = nn.Sequential(
-             	nn.ConvTranspose1d(64, 32, kernel_size=4, stride=1, padding=0, bias=False),
-            	nn.BatchNorm1d(32),
-            	nn.ReLU(True),
-            )
+                 	nn.ConvTranspose1d(64, 32, kernel_size=4, stride=1, padding=0, bias=False),
+                	nn.BatchNorm1d(32),
+                	nn.ReLU(True),
+                )
 		self.cnnTrans2 = nn.Sequential(
-             	nn.ConvTranspose1d(32, 16, kernel_size=2, stride=1, padding=0, bias=False),
-            	nn.BatchNorm1d(16),
-            	nn.ReLU(True),
-            )
+                 	nn.ConvTranspose1d(32, 16, kernel_size=2, stride=1, padding=0, bias=False),
+                	nn.BatchNorm1d(16),
+                	nn.ReLU(True),
+                )
 		self.cnnTransOut = nn.Sequential(
              	nn.Conv1d(16, 1, kernel_size=1, stride=1, padding=0, bias=False),
             )
@@ -313,83 +337,96 @@ class CNNGenerator(nn.Module):
 		# print(1, out.shape)
 		out = self.avgPool2(out)
 		# print(2, out.shape)
-		res2 = self.cnn2(out)
-		out = res2
-
+		out = self.cnn2(out)
 		# print(3, out.shape)
-		out = self.avgPool2(out)
-		# print(4, out.shape)
-		res4 = self.cnn4(out)
-		out = res4
+		res22 = self.cnn22(out)
+		out = res22
 
-		# print(5, out.shape)
+		# print(4, out.shape)
 		out = self.avgPool2(out)
+		# print(5, out.shape)
+		out = self.cnn4(out)
 		# print(6, out.shape)
-		res8 = self.cnn8(out)
-		out = res8
+		res44 = self.cnn44(out)
+		out = res44
 
 		# print(7, out.shape)
 		out = self.avgPool2(out)
 		# print(8, out.shape)
-		res16 = self.cnn16(out)
-		out = res16
-
+		out = self.cnn8(out)
 		# print(9, out.shape)
-		out = self.avgPool2(out)
-		# print(10, out.shape)
-		res32 = self.cnn32(out)
-		out = res32
+		res88 = self.cnn88(out)
+		out = res88
 
-		# print(11, out.shape)
+		# print(10, out.shape)
 		out = self.avgPool2(out)
+		# print(11, out.shape)
+		out = self.cnn16(out)
 		# print(12, out.shape)
-		out = self.cnn64(out)
+		res1616 = self.cnn1616(out)
+		out = res1616
+
 		# print(13, out.shape)
+		out = self.avgPool2(out)
+		# print(14, out.shape)
+		out = self.cnn32(out)
+		# print(15, out.shape)
+		res3232 = self.cnn3232(out)
+		out = res3232
+
+		# print(16, out.shape)
+		out = self.avgPool2(out)
+		# print(17, out.shape)
+		out = self.cnn64(out)
+		# print(18, out.shape)
 
 		out = self.bridge1(out)
-		# print(14, out.shape)
+		# print(19, out.shape)
 		out = self.bridge2(out)
-		# print(15, out.shape)
+		# print(20, out.shape)
 		out = self.bridge3(out)
-		# print(16, out.shape)
+		# print(21, out.shape)
 
 		out = self.cnnTrans64(out)
-		# print(17, out.shape)
+		# print(22, out.shape)
 		out = self.upsample2(out)
-		# print(18, out.shape)
-		out = out + res32
+		# print(23, out.shape)
+		out = out + res3232
 
 		out = self.cnnTrans32(out)
-		# print(19, out.shape)
+		out = self.cnn3232(out)
+		# print(24, out.shape)
 		out = self.upsample2(out)
-		# print(20, out[:,:,0:-1].shape)
-		# print(20, res16.shape)
-		out = out[:,:,0:-1] + res16
+		# print(25, out.shape)
+		out = out[:,:,0:-1] + res1616
 
 		out = self.cnnTrans16(out)
-		# print(21, out.shape)
+		out = self.cnn88(out)
+		# print(26, out.shape)
 		out = self.upsample2(out)
-		# print(22, out.shape)
-		out = out + res8
+		# print(27, out.shape)
+		out = out + res88
 
 		out = self.cnnTrans8(out)
-		# print(23, out.shape)
+		out = self.cnn44(out)
+		# print(28, out.shape)
 		out = self.upsample2(out)
-		# print(24, out.shape)
-		out = out[:,:,0:-1] + res4
+		# print(29, out.shape)
+		out = out[:,:,0:-1] + res44
 
 		out = self.cnnTrans4(out)
-		# print(25, out.shape)
+		out = self.cnn22(out)
+		# print(30, out.shape)
 		out = self.upsample2(out)
-		# print(26, out.shape)
-		out = out[:,:,0:-1] + res2
+		# print(31, out.shape)
+		out = out[:,:,0:-1] + res22
 
 		out = self.cnnTrans2(out)
-		# print(27, out.shape)
+		# print(32, out.shape)
 		out = self.upsample2(out)
-		# print(28, out.shape)
+		# print(33, out.shape)
 		out = out + res1
 
 		out = self.cnnTransOut(out)
-		# print(29, out.shape)
+		# print(34, out.shape)
 		return out
