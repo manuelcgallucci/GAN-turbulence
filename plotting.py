3ea1afa81fdbce927f896799e090a995f64c6fd5
Manuel CABEZA GALLUCCI <m21cabez@sl-tp-br-023.imta.fr>
2023-02-21 17:51:04 +0100

    Added hitogram plotting

--- plotting.py
+++ plotting.py
@@ -11,7 +11,8 @@ display = False
 
 def main():
     # plot_real_data()
-    plot_compare_structure()
+    # plot_compare_structure(eta=6, L=2375)
+    plot_histogram(n_samples=64, scales=[2,4,8,16,128,256,1024,4096,8192,16384])
     # plot_structure()
     # plot_history()
     # plot_samples()
@@ -137,8 +138,9 @@ def plot_structure(n_samples=64, len_=2**15, edge=4096, device="cuda"):
     # if save: plt.savefig(data_dir + "s2_cumsum_samples.png")
     # if display: plt.show()
 
+### ======================================= ###
 
-def plot_compare_structure(n_samples=64, len_=2**15, edge=4096, device="cuda"):
+def plot_compare_structure(n_samples=64, len_=2**15, edge=4096, eta=None, L=None, device="cuda"):
     data_train = np.load('./data/data.npy')
 
     nv=10
@@ -155,41 +157,65 @@ def plot_compare_structure(n_samples=64, len_=2**15, edge=4096, device="cuda"):
     generated_samples = generated_samples[:,:,edge:-edge]
     log_scale = np.log(scales)
     
-    
     struct = ut.calculate_structure(generated_samples, scales, device=device)
     struct_mean_generated = torch.mean(struct[:,:,:], dim=0).cpu()
-    
+    struct_std_generate = torch.std(struct[:,:,:], dim=0).cpu()
+
     struct = ut.calculate_structure(torch.Tensor(data_train[:,None,:]), scales, device=device)
     struct_mean_real = torch.mean(struct[:,:,:], dim=0).cpu()
+    struct_std_real = torch.std(struct[:,:,:], dim=0).cpu()
 
-    for idx, k in enumerate(range(2,5)):
+    names = ["s2", "skewness", "flatness"]
+    ylabel = ["log(s2)", "skewness", "log(F / 3) - verify the 3"]
+    vlines_lim = [(-7, 1), (-0.7, 0.1), (-0.2, 1)]
+    for idx in range(3):
         plt.figure()
-        plt.plot(log_scale, struct_mean_generated[idx,:], 'r', linewidth=2.0)
-        plt.plot(log_scale, struct_mean_real[idx,:], linewidth=2.0)
-        plt.title("Structure function s{:d} on the samples".format(k))
+        plt.errorbar(log_scale, struct_mean_generated[idx,:], struct_std_generate[idx,:], color="red",  linewidth=2.0)
+        plt.errorbar(log_scale, struct_mean_real[idx,:], struct_std_real[idx,:], linewidth=2.0, alpha=0.5)
+        plt.title("Structure function {:s} on the samples".format(names[idx]))
         plt.xlabel("scales (log)")
+        plt.ylabel(ylabel[idx])
         plt.xticks([x for x in range(0, int(np.ceil(log_scale[-1])))])
         plt.legend(["Generated", "Real"])
+        plt.vlines(np.log(L), vlines_lim[idx][0], vlines_lim[idx][1], color='k', linestyle='--', linewidth=2.5, alpha=0.8)
+        plt.vlines(np.log(eta), vlines_lim[idx][0], vlines_lim[idx][1], color='k', linestyle='--', linewidth=2.5, alpha=0.8)
         plt.grid()
-        if save: plt.savefig(data_dir + "s{:d}_comparison.png".format(k))
+        if save: plt.savefig(data_dir + "comparison_{:s}.png".format(names[idx]))
         if display: plt.show()
 
-    # s2 = ut.calculate_s2(generated_samples, scales, device=device)
-    # s2_mean_generated = torch.mean(s2[:,0,:], dim=0).cpu()
-    
-    # s2 = ut.calculate_s2(torch.Tensor(data_train[:,None,:]), scales, device=device)
-    # s2_mean_real = torch.mean(s2[:,0,:], dim=0).cpu()
+### ======================================= ###
 
-    # plt.figure()
-    # plt.plot(log_scale, s2_mean_generated, 'r', linewidth=2.0)
-    # plt.plot(log_scale, s2_mean_real, linewidth=2.0)
-    # plt.title("Structure function s on the samples")
-    # plt.xlabel("scales (log)")
-    # plt.xticks([x for x in range(0, int(np.ceil(log_scale[-1])))])
-    # plt.legend(["Generated", "Real"])
-    # plt.grid()
-    # if save: plt.savefig(data_dir + "s2_comparison.png")
-    # if display: plt.show()
+def plot_histogram(n_samples=64, len_=2**15, edge=4096, n_bins=150, scales=[2**x for x in range(1,14)], device="cuda"):
+    # generator = CNNGenerator().to(device)
+    # generator.load_state_dict(torch.load(data_dir + 'generator.pt'))
+    # noise = torch.randn((n_samples, 1, len_+2*edge), device=device)
+    # with torch.no_grad():
+    #     generated_samples = generator(noise)
+    
+    # generated_samples = generated_samples[:,:,edge:-edge].cpu()
+    
+    generated_samples = torch.Tensor(np.load('./data/data.npy')[:,None,:])
+    histograms, bins = ut.calculate_histogram(generated_samples, scales, n_bins, device="cpu") 
+    histograms = np.log(histograms + 1e-5)
+
+    histgn, binsg = np.histogram(np.random.randn(n_samples*len_), bins=n_bins, density=True)
+    # tuple to set the limits of the scales and color
+    params_ = [(32, "Dissipative", "black"), (512, "Inertial", "blue"), (32768, "Dissipative", "red")]
+    delta_hist = 2
+    plt.figure(figsize=(6,6))
+    for idx, scale in enumerate(scales):
+        param_idx = np.argmax(np.array([scale < param[0] for param in params_]) > 0)
+        plt.plot(bins[0:-1], delta_hist * (-idx) + histograms[idx,:], color=params_[param_idx][2], linewidth=1.5, label="sc:"+str(scale))
+    plt.plot(binsg[0:-1],delta_hist * (-len(scales)) + np.log(histgn), "--", color="k", linewidth=1.5, alpha=0.8, label="Gaussian")
+    plt.title("Histogram")
+    plt.legend(bbox_to_anchor = (1.25, 0.6), loc='center right')
+    plt.yticks([])
+    plt.ylabel(r'$\log(P(\delta^{\prime}_l u(x)))$')
+    plt.xlabel(r'$\delta^{\prime}_l u(x)$')
+    plt.grid()
+    plt.tight_layout()
+    if save: plt.savefig(data_dir + "histogram_real.png")
+    if display: plt.show()
 
 ### ======================================= ###
 
