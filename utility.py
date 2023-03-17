f4b175c1d79560a891d82229a5fc012fb5d490c2
Manuel CABEZA GALLUCCI <m21cabez@vl-tp-br-021.imta.fr>
2023-03-17 17:53:17 +0100

    added training structure function calculation

--- utility.py
+++ utility.py
@@ -55,7 +55,39 @@ def calculate_structure(signal, scales, device="cpu"):
 
     return structure_f
 
+def calculate_structure_noInplace(signal, scales, device="cpu"):
+    '''
+    signal is the signal of study and scales is an array with the values of the scales of analysis
+    '''      
+    # This contains in order, log(s2), skewness, log(flatness / 3)
+    structure_f = torch.zeros((signal.shape[0],3,len(scales)), dtype=torch.float32, device=device)
 
+    # We normalize the image by centering and standarizing it
+    Nreal=signal.size()[0]
+    tmp = torch.zeros(signal.shape, device=device)    
+    for ir in range(Nreal):
+        nanstdtmp = torch.sqrt(torch.nanmean(torch.abs(signal[ir]-torch.nanmean(signal[ir]))**2))
+        tmp[ir,0,:] = (signal[ir]-torch.nanmean(signal[ir]))/nanstdtmp   
+
+    for idx, scale in enumerate(scales):
+        incrs = tmp[:,0,scale:]-tmp[:,0,:-scale]
+        structure_f[:,0,idx] = torch.mean(torch.square(incrs), dim=1)
+        
+        stdincrs = torch.std(incrs, dim=1)
+        incrsnorm = (incrs - torch.nanmean(incrs, dim=1)[:,None]) / stdincrs[:,None] # Batch x Length
+        
+        structure_f[:,1,idx] = torch.nanmean(torch.pow(incrsnorm,3), dim=1)
+        structure_f[:,2,idx] = torch.nanmean(torch.pow(incrsnorm, 4), dim=1)
+
+    # log s2 and log flatness / 3
+    #log_s2 = torch.log(structure_f[:,0,:])
+    #structure_f[:,0,:] = log_s2
+    
+    #log_flatness = torch.log(structure_f[:,2,:] / 3)
+    #structure_f[:,2,:] = log_flatness
+    
+    return structure_f
+    
 # Device has to be cpu since histogram is not yet implemented on CUDA
 def calculate_histogram(signal, scales, n_bins, device="cpu", normalize_incrs=True):
     if device != "cpu":
