7d9ed067b4071a10a0e15f8aa28c55b6e6b34703
Manuel Cabeza Gallucci <42854238+manuelcgallucci@users.noreply.github.com>
2023-04-13 19:59:15 +0200

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -1,40 +1,52 @@
 import torch
+import os
+import numpy as np
+
+import utility as ut
 import nn_definitions as nn_d
+from model_generator import CNNGeneratorBigConcat as CNNGenerator
 
-from model_generator import CNNGeneratorBCNocnn1 as CNNGenerator
-data_dir = './generated/HenoXL/'
+model_names = ["mFwn3R","EDDVg4","1mnY3T","4K9Vke","6SgDhO","13whmJ","ORUKrz","k5cpTg","cyy81t","3fzNLY","XZ9Nph","2i5EIu","f5tOXl","VUwRRy","zXxm1p","VTsm0o","aKbLgh","7PU6YZ","Bnh0Pu"]
 
-def test_model(n_samples=64, len_=2**15, edge=4096, device="cuda"):
+def test_model(scales, model_id=None, n_samples=64, len_=2**15, edge=4096, device="cuda"):
+    data_dir = os.path.join('./generated', model_id)
+    generator_dir = os.path.join(data_dir, "generator.pt")
+
+    if not os.path.exists(generator_dir):
+        print("Model {:s} doesnt exist!".format(model_id))
+
+    # Load the model 
     generator = CNNGenerator().to(device)
-    generator.load_state_dict(torch.load(data_dir + 'generator.pt'))
+    generator.load_state_dict(torch.load(generator_dir))
     
-    discriminator = nn_d.Discriminator().to(device)
-    discriminator.load_state_dict(torch.load(data_dir + 'discriminator.pt'))
-
+    # Generate the samples
     noise = torch.randn((n_samples, 1, len_+2*edge), device=device)
     with torch.no_grad():
         generated_samples = generator(noise)
         generated_samples = generated_samples[:,:,edge:-edge]
 
-        predictions_edge = discriminator(generated_samples)
-    acc_edge = torch.sum(predictions_edge < 0.5).item() / n_samples
+    # Generate the structure functions from the data 
 
-
-
-    noise = torch.randn((n_samples, 1, len_), device=device)
-    with torch.no_grad():
-        generated_samples = generator(noise)
-        generated_samples = generated_samples
-
-        predictions = discriminator(generated_samples)
-    acc = torch.sum(predictions < 0.5).item() / n_samples
-
-
-    print("Discriminator accuracy on the results:")
-    print("\tWith edge: {:4.3f}".format(acc_edge))
-    print("\t\tMax / Min prediction: {:3.2f} / {:3.2f}".format(torch.max(predictions_edge).item(), torch.min(predictions_edge).item()))
-    print("\tNo edge: {:4.3f}".format(acc))
-    print("\t\tMax / Min prediction: {:3.2f} / {:3.2f}".format(torch.max(predictions).item(), torch.min(predictions).item()))
+    struct = ut.calculate_structure(generated_samples, scales, device=device)
+    struct_mean_generated = torch.mean(struct[:,:,:], dim=0)
+    return struct_mean_generated
+   
 
 if __name__ == "__main__":
-    test_model()
+    print("Order: S2, Skewness, Flatness")
+    
+    device="cuda"
+
+    nv=10
+    uu=2**np.arange(0,13,1/nv)
+    scales=np.unique(uu.astype(int))
+    scales=scales[0:100]
+
+    data_train = np.load('./data/data.npy')
+    struct_mean_real = ut.calculate_structure(torch.Tensor(data_train[:,None,:]), scales, device=device)
+    struct_mean_real = torch.mean(struct_mean_real[:,:,:], dim=0)
+    
+    for model_name in model_names:
+        struct_mean_generated=test_model(scales, model_id=model_name, device=device)
+        mse_structure = torch.mean(torch.square(struct_mean_generated - struct_mean_real), dim=1)
+        print(model_name, mse_structure)
