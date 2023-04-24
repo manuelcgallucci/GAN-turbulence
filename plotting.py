68d555fcc39e7217437a28ea6e8340406eaeb8ee
Manuel CABEZA GALLUCCI <m21cabez@sl-tp-br-021.imta.fr>
2023-04-24 18:44:13 +0200

    Improved plotting

--- plotting.py
+++ plotting.py
@@ -14,7 +14,7 @@ from model_generator import CNNGeneratorBigConcat as CNNGenerator
 # from model_discriminator import DiscriminatorMultiNet16 as Discriminator
 #data_dir = './generated/U2lVlk/'
 
-data_dir = "./generated/6SgDhO/"
+data_dir = "./generated/guL8Zi/"
 temp_dir = "./temp/"
 save = True
 display = False
@@ -30,8 +30,8 @@ def main():
 	# plot_history()
 
 	# plot_samples()
-	plot_compare_structure(eta=5, L=2350, n_samples=200, n_batches=16, len_=samples_len)
-	plot_histogram(n_samples=64, len_=samples_len, scales=[2,4,8,16,128,256,1024,4096,8192,16384])
+	plot_compare_structure(eta=5, L=2350, n_samples=128, n_batches=16, len_=samples_len)
+	# plot_histogram(n_samples=64, len_=samples_len, scales=[2,4,8,16,128,256,1024,4096,8192,16384])
 
 	# plot_training_samples(eta=5, L=2350)
 	# plot_structure()
@@ -309,68 +309,75 @@ def plot_structure(n_samples=64, len_=2**15, edge=4096, device="cuda"):
 ### ======================================= ###
 
 def plot_compare_structure(n_samples=64, n_batches=2, len_=2**15, edge=4096, eta=None, L=None, device="cuda"):
-    data_train = np.load('./data/data.npy')
+	data_train = np.load('./data/data.npy')
 
-    nv=10
-    uu=2**np.arange(0,13,1/nv)
-    scales=np.unique(uu.astype(int))
-    scales=scales[0:100]
+	nv=10
+	uu=2**np.arange(0,13,1/nv)
+	scales=np.unique(uu.astype(int))
+	scales=scales[0:100]
 
-    generator = CNNGenerator().to(device)
-    generator.load_state_dict(torch.load(data_dir + 'generator.pt'))
-    
-    struct_means_g = torch.zeros((3, scales.shape[0]), device=device)
-    for i_batch in range(n_batches):
-        noise = torch.randn((n_samples, 1, len_+2*edge), device=device)
-        with torch.no_grad():
-            generated_samples = generator(noise)
-        generated_samples = generated_samples[:,:,edge:-edge]
-        
-        # for i in range(n_samples):
-        #     generated_samples[i,0,:] = np.flip(generated_samples[i,0,:])
-        generated_samples = generated_samples[:,0,:]
-        
-        generated_samples = np.flip(np.array(generated_samples.cpu()), axis=1).copy()
-        generated_samples = generated_samples[:,None,:]
-        generated_samples = torch.Tensor(generated_samples).to(device)
-        # generated_samples = torch.Tensor(np.load(data_dir + 'samples.npz')["arr_0"])
-        
-        struct = ut.calculate_structure(generated_samples, scales, device=device)
-        struct_mean_generated = torch.mean(struct[:,:,:], dim=0)
-        struct_means_g += struct_mean_generated
-        struct_std_generate = torch.std(struct[:,:,:], dim=0).cpu()
+	generator = CNNGenerator().to(device)
+	generator.load_state_dict(torch.load(data_dir + 'generator.pt'))
 
-    struct_means_g = struct_means_g.cpu() / n_batches
-    log_scale = np.log(scales)
+	struct_means_g = torch.zeros((3, scales.shape[0]), device=device)
+	for i_batch in range(n_batches):
+		noise = torch.randn((n_samples, 1, len_+2*edge), device=device)
+		with torch.no_grad():
+			generated_samples = generator(noise)
+		generated_samples = generated_samples[:,:,edge:-edge]
+		
+		# for i in range(n_samples):
+		#     generated_samples[i,0,:] = np.flip(generated_samples[i,0,:])
+		generated_samples = generated_samples[:,0,:]
+		
+		generated_samples = np.flip(np.array(generated_samples.cpu()), axis=1).copy()
+		generated_samples = generated_samples[:,None,:]
+		
+		fig = plt.figure()
+		for k in range(generated_samples.shape[0]):
+			plt.plot(generated_samples[k, 0, :], linewidth=1.0)
+		fig.suptitle('Generated samples')
+		plt.savefig(data_dir + "sample_once.png")
+		
+		generated_samples = torch.Tensor(generated_samples).to(device)
+		# generated_samples = torch.Tensor(np.load(data_dir + 'samples.npz')["arr_0"])
+		
+		struct = ut.calculate_structure(generated_samples, scales, device=device)
+		struct_mean_generated = torch.mean(struct[:,:,:], dim=0)
+		struct_means_g += struct_mean_generated
+		struct_std_generate = torch.std(struct[:,:,:], dim=0).cpu()
 
-    # TODO correct the data trin so this is not needed anymore
-    data_train = np.flip(data_train, axis=1).copy()
-    struct = ut.calculate_structure(torch.Tensor(data_train[:,None,:]), scales, device=device)
-    struct_mean_real = torch.mean(struct[:,:,:], dim=0).cpu()
-    struct_std_real = torch.std(struct[:,:,:], dim=0).cpu()
+	struct_means_g = struct_means_g.cpu() / n_batches
+	log_scale = np.log(scales)
 
-    names = ["s2", "skewness", "flatness"]
-    ylabel = ["log(s2)", "skewness", "log(F / 3) - verify the 3"]
-    vlines_lim = [(-7, 1), (-0.7, 0.1), (-0.2, 1)]
-    for idx in range(3):
-        plt.figure()
-        plt.errorbar(log_scale, struct_means_g[idx,:], struct_std_generate[idx,:], color="red",  linewidth=2.0)
-        plt.errorbar(log_scale, struct_mean_real[idx,:], struct_std_real[idx,:], linewidth=2.0, alpha=0.5)
-        plt.title("Structure function {:s} on the samples".format(names[idx]))
-        plt.xlabel("scales (log)")
-        plt.ylabel(ylabel[idx])
-        plt.xticks([x for x in range(0, int(np.ceil(log_scale[-1])))])
-        plt.legend(["Generated", "Real"])
-        plt.vlines(np.log(L), vlines_lim[idx][0], vlines_lim[idx][1], color='k', linestyle='--', linewidth=2.5, alpha=0.8)
-        plt.vlines(np.log(eta), vlines_lim[idx][0], vlines_lim[idx][1], color='k', linestyle='--', linewidth=2.5, alpha=0.8)
-        plt.grid()
-        if save: plt.savefig(data_dir + "comparison_{:s}.png".format(names[idx]))
-        if display: plt.show()
+	# TODO correct the data trin so this is not needed anymore
+	data_train = np.flip(data_train, axis=1).copy()
+	struct = ut.calculate_structure(torch.Tensor(data_train[:,None,:]), scales, device=device)
+	struct_mean_real = torch.mean(struct[:,:,:], dim=0).cpu()
+	struct_std_real = torch.std(struct[:,:,:], dim=0).cpu()
 
-    # Compute and save structure function metrics
-    mse_structure = torch.mean(torch.square(struct_means_g - struct_mean_real), dim=1)
-    print(mse_structure)
-    
+	names = ["s2", "skewness", "flatness"]
+	ylabel = ["log(s2)", "skewness", "log(F / 3) - verify the 3"]
+	vlines_lim = [(-7, 1), (-0.7, 0.1), (-0.2, 1)]
+	for idx in range(3):
+		plt.figure()
+		plt.errorbar(log_scale, struct_means_g[idx,:], struct_std_generate[idx,:], color="red",  linewidth=2.0)
+		plt.errorbar(log_scale, struct_mean_real[idx,:], struct_std_real[idx,:], linewidth=2.0, alpha=0.5)
+		plt.title("Structure function {:s} on the samples".format(names[idx]))
+		plt.xlabel("scales (log)")
+		plt.ylabel(ylabel[idx])
+		plt.xticks([x for x in range(0, int(np.ceil(log_scale[-1])))])
+		plt.legend(["Generated", "Real"])
+		plt.vlines(np.log(L), vlines_lim[idx][0], vlines_lim[idx][1], color='k', linestyle='--', linewidth=2.5, alpha=0.8)
+		plt.vlines(np.log(eta), vlines_lim[idx][0], vlines_lim[idx][1], color='k', linestyle='--', linewidth=2.5, alpha=0.8)
+		plt.grid()
+		if save: plt.savefig(data_dir + "comparison_{:s}.png".format(names[idx]))
+		if display: plt.show()
+
+	# Compute and save structure function metrics
+	mse_structure = torch.mean(torch.square(struct_means_g - struct_mean_real), dim=1)
+	print(mse_structure)
+
 ### ======================================= ###
 
 def plot_histogram(n_samples=64, len_=2**15, edge=4096, n_bins=150, scales=[2**x for x in range(1,14)], device="cuda"):
